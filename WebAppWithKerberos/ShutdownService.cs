bfbcaa1122e963909ec6a6c5a9ab472be46a97ab
Anton Bandurka <aabandurka@yandex.ru>
2019-12-09 23:14:22 +0300

    Add project files.

---
+++ WebAppWithKerberos/ShutdownService.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppWithKerberos
+{
+    public class ShutdownService: IHostedService
+    {
+        private readonly ILogger _logger;
+
+        public ShutdownService(ILogger<ShutdownService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{nameof(ShutdownService)} started");
+            return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{nameof(ShutdownService)} is stopping");
+
+            for (var i = 0; i < 5; i++)
+            {
+                _logger.LogInformation($"{nameof(ShutdownService)} is stopping { i } sec");
+                await Task.Delay(1000);
+            }
+        }
+    }
+
+    public class KerberosService : IHostedService
+    {
+        private readonly ILogger _logger;
+
+        public KerberosService(ILogger<KerberosService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{nameof(KerberosService)} started");
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                _logger.LogInformation($"It's linux!");
+                BuildLinuxKerberosSchedule();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private void BuildLinuxKerberosSchedule()
+        {
+            // setup cron schedule
+
+            // run kinit.sh
+
+            //var strCmdText = "/C copy /b Image1.jpg + Archive.rar Image2.jpg";
+            //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ WebAppWithKerberos/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace WebAppWithKerberos
+{
+    public class Startup
+    {
+        private ILogger _logger;
+        public IConfiguration Configuration { get; }
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private void onStarted()
+        {
+            _logger?.LogInformation("App started successfully!");
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddLogging(c=> c.AddConsole());
+
+            services.AddHostedService<ShutdownService>();
+            services.AddHostedService<KerberosService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLife, ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger("StartupLogger");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+            appLife.ApplicationStarted.Register(onStarted);
+
+        }
+    }
+}
